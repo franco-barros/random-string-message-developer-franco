55d65b8205bec2021204d5d12289eb85c44b9868
Franco Barros <fb6776928@gmail.com>
2024-10-28 19:53:03 -0300

    superchange

--- src/index.js
+++ src/index.js
@@ -10,8 +10,18 @@ const messages = [
   "Here be Dragons",
   "Reinventing the wheel. Again.",
   "This is not the commit message you are looking for",
+  "Superman its cool",
   "Batman! (this commit has no parents)",
+  "deadpool its the best",
   "¡Hoy es un gran día para aprender algo nuevo!",
+  "With great power comes great responsibility. Spider-Man",
+  "It's not who I am underneath, but what I do that defines me. Batman",
+  "I can do this all day. Captain America",
+  "The answers you seek shall be yours, once I claim whats mine. Thor",
+  "I am Iron Man.  Iron Man",
+  "Heroes are made by the path they choose, not the powers they are graced with. Green Lantern",
+  "In brightest day, in blackest night, no evil shall escape my sight. Green Lantern",
+  "Sometimes you have to run before you can walk. Iron Man",
 ];
 
 const funnyCommit = () => {
